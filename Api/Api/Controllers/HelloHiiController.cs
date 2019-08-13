e313fe524de6d46f6caa531d274b09b4d4d2819b
tavisca-rtiwari <51901929+tavisca-rtiwari@users.noreply.github.com>
2019-08-13 18:59:13 +0530

    Add files via upload

---
+++ Api/Api/Controllers/HelloHiiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloHiiController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Something went wrong";
+        }
+        [HttpGet]
+        public string Get(string input)
+        {
+            if (input.Equals("Hii"))
+            {
+                return "Hello";
+            }
+            else if (input.Equals("Hello"))
+            {
+                return "Hii";
+            }
+            return "Something went wrong";
+        }
+    }
+}
